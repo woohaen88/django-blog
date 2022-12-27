d2829ebe33032c3ee335f2ac0cca841f03ccbb32
woohaen88 <woohaen88@gmail.com>
2022-12-27 22:22:57 +0900

    Feat: FBV -> CBV 전환

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
-def index(request):
-    posts = Post.objects.all().order_by("-pk")
-    return render(request, "blog/post_list.html", {"posts": posts})
+class PostList(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    ordering = "-pk"
+    context_object_name = "post_list"
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(request, "blog/single_post_page.html", {"post": post})
+class PostDetail(DetailView):
+    model = Post
+    context_object_name = "post"
+    template_name = "blog/single_post_page.html"
