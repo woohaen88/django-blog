017f31e4e3bf939108aa7d78905de58a8b12ee98
woohaen88 <woohaen88@gmail.com>
2022-12-28 00:43:53 +0900

    Feat: 포스트 목록 상세 페이지에서 대표 이미지 없을  때 문제 해결

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,10 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to="blog/images/%Y/%m/%d/", blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"[{self.pk}] {self.title}"
