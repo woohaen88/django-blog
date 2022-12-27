a58a8df3784f134b7ee46c577483b956e23a63fd
woohaen88 <woohaen88@gmail.com>
2022-12-27 21:12:11 +0900

    Feat: 대문 페이지와 자기소개 페이지

--- configuration/urls.py
+++ configuration/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("blog", include("blog.urls")),
+    path("blog/", include("blog.urls")),
     path("admin/", admin.site.urls),
+    path("", include("single_pages.urls")),
 ]
