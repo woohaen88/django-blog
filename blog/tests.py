81bdf2ff699a547db07043d69b8998909c5bf709
whkim88 <whkim88@lotte.net>
2022-12-28 15:06:57 +0900

    Feat: Blog 상세페이지까지 TDD 구현

--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,31 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        # 1.5. Blog, About Me라는 문구가 내비게이션 바에 있다.
+        self.assertIn("Home", navbar.text)
+        self.assertIn("Blog", navbar.text)
+        self.assertIn("About Me", navbar.text)
+        self.assertIn("Contact", navbar.text)
+
+
+        # test_button
+        test_button_link = {
+            "woohaen Blog": "/",
+            "Home": "/",
+            "About Me": "/about_me/",
+            # "Contact": "/",
+            "Blog": "/blog/",
+        }
+        # Logo
+        for (key, value) in test_button_link.items():
+            btn = navbar.find("a", text=key)
+            self.assertEqual(btn.attrs["href"], value)
+        # logo_btn = navbar.find("a", text="woohaen Blog")
+        # self.assertEqual(logo_btn.attrs["href"], "/")
+
+
     def test_post_list(self):
         # 1.1. 포스트 목록 페이지를 가져온다.
         response = self.client.get("/blog/")
@@ -16,10 +41,7 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, "html.parser")
         self.assertEqual(soup.title.text, "Blog")
         # 1.4. 내비게이션 바가 있다.
-        navbar = soup.nav
-        # 1.5. Blog, About Me라는 문구가 내비게이션 바에 있다.
-        self.assertIn("Blog", navbar.text)
-        self.assertIn("About Me", navbar.text)
+        self.navbar_test(soup)
 
         # 2.1. 메인 영역에 게시물이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -56,9 +78,7 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, "html.parser")
 
         # 2.2. 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn("Blog", navbar.text)
-        self.assertIn("About Me", navbar.text)
+        self.navbar_test(soup)
 
         # 2.3. 첫번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
         self.assertIn(post_001.title, soup.title.text)
